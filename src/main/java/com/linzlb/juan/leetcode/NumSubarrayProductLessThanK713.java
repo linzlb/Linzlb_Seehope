ee2f20701b746eda28f112422eba673ed985dd8f
jiangwan <jiangwan.lin@tuya.com>
2022-05-05 11:08:12 +0800

    Linzlb#V1.5_msg:ThreadLocal错误内容修改

---
+++ src/main/java/com/linzlb/juan/leetcode/NumSubarrayProductLessThanK713.java
@@ -0,0 +1,34 @@
+package com.linzlb.juan.leetcode;
+
+/**
+ * 713. 乘积小于 K 的子数组
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目。
+ * eg.
+ * 输入：nums = [10,5,2,6], k = 100
+ * 输出：8
+ * 解释：8 个乘积小于 100 的子数组分别为：[10]、[5]、[2],、[6]、[10,5]、[5,2]、[2,6]、[5,2,6]。
+ * 需要注意的是 [10,5,2] 并不是乘积小于 100 的子数组。
+ * 链接：https://leetcode-cn.com/problems/subarray-product-less-than-k
+ *
+ * 思路：
+ * 滑动窗口
+ *
+ * @author jiangwan.lin@tuya.com
+ * @since 2022-05-05 10:39 上午 / V1.0
+ */
+public class NumSubarrayProductLessThanK713 {
+
+    public int solution(int[] nums, int k) {
+        int n = nums.length, ret = 0;
+        int prod = 1, i = 0;
+        for (int j = 0; j < n; j++) {
+            prod *= nums[j];
+            while (i <= j && prod >= k) {
+                prod /= nums[i];
+                i++;
+            }
+            ret += j - i + 1;
+        }
+        return ret;
+    }
+}
