b7eeaae854de9b87007077dca25817e06b4b52ca
jiangwan <jiangwan.lin@tuya.com>
2022-06-02 15:24:42 +0800

    Linzlb#V3.8_msg:测试序列化

--- src/test/java/com/tuya/demo/TestJsonInclude.java
+++ src/test/java/com/tuya/demo/TestJsonInclude.java
@@ -1,10 +1,15 @@
 package com.tuya.demo;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 
 public class TestJsonInclude {
@@ -16,6 +21,34 @@ public class TestJsonInclude {
                 .c(null).build();
         System.out.println(JSON.toJSONString(data, SerializerFeature.WriteMapNullValue));
         System.out.println(JSON.toJSONString(data));
+
+
+        //测试经过了网关后暴露出去的，还也没有null字段
+        //结论：找不到了
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            String url = "http://smarthome-api-dev.coli688.com/idueros/third-center/callback";
+            String req = "{\n" +
+                    "    \"header\":{\n" +
+                    "        \"payloadVersion\":\"1\",\n" +
+                    "        \"name\":\"DiscoverAppliancesRequest\",\n" +
+                    "        \"namespace\":\"DuerOS.ConnectedHome.Discovery\",\n" +
+                    "        \"messageId\":\"_Smarthome_617f9065561536.34776942\"\n" +
+                    "    },\n" +
+                    "    \"payload\":{\n" +
+                    "        \"accessToken\":\"kOZjpuIRaSuEdy8iXrAGGDMwCEXDirpxmyOSjgF8aquH4fg94N\",\n" +
+                    "        \"openUid\":\"linzlb\"\n" +
+                    "    }\n" +
+                    "}";
+            Object reqObj = JSONObject.parseObject(req);
+            ResponseEntity<Object> response = restTemplate.postForEntity(url, reqObj, Object.class);
+            Object responseBody = response.getBody();
+            System.out.println(JSON.toJSONString(responseBody, SerializerFeature.WriteMapNullValue));
+
+        } catch (RestClientException e) {
+            e.printStackTrace();
+        }
     }
 
 }
